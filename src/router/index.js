a10647939b25d7b3153472e550b73e547a5dc567
guo1638449141 <1638449141@qq.com>
2021-04-25 20:16:32 +0800

    配置营销路由

--- src/router/index.js
+++ src/router/index.js
@@ -234,6 +234,40 @@ export const asyncRoutes = [
       }
     ]
   },
+  {
+      path:"/marketing",
+      component:Layout,
+      meta:{
+          title:"营销",
+          icon:"education"
+      },
+      redirect:"/order",
+      children: [{
+        path: "group",
+        name: "Group",
+        component: () =>
+            import ('@/views/marketing/group'),
+        meta: {
+            title: "拼团"
+        }
+    }, {
+        path: "flashsale",
+        name: "Flashsale",
+        component: () =>
+            import ('@/views/marketing/flashsale'),
+        meta: {
+            title: "秒杀"
+        }
+    }, {
+        path: "coupon",
+        name: "Coupon",
+        component: () =>
+            import ('@/views/marketing/coupon'),
+        meta: {
+            title: "优惠券"
+        }
+    }]
+  },
   //   {
   //     path: '/permission',
   //     component: Layout,
